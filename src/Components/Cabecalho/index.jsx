c7d0ad1f44c2049c546d29af194a7e701606c219
Danielle Souza <danielle.andrade@meta.com.br>
2021-12-21 13:36:22 -0300

    feat: mudanças estilo

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
+import { corPrimaria } from "../UI/Variaveis";
+
+const StyledHeader = styled.nav`
+.cabecalho {
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+}
+`
+const Logo = styled.img`
+.imagem-logo {
+  height: 50px;
+  width: 50px;
+}
+
+`
 
 const Cabecalho = () => {
   return (
+    <StyledHeader>
     <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secundario" href="https://google.com">
           Ajuda
@@ -14,6 +35,7 @@ const Cabecalho = () => {
         </a>
       </div>
     </div>
+    </StyledHeader>
   );
 };
 
